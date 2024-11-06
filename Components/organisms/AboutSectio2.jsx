d4c89fa52da1417c4cc8817a0b89e34aed6c7d72
Ayesha Mukhtar <144342578+AYESHA4356@users.noreply.github.com>
2024-11-06 22:58:37 +0500

    complete

--- Components/organisms/AboutSectio2.jsx
+++ Components/organisms/AboutSectio2.jsx
@@ -7,22 +7,22 @@ function AboutSection2() {
         <div className="flex my-[10%] mx-[5%] ">
 
             <div className=" w-[31%] mx-[7.5%]  mb-2 shadow-lg rounded-xl bg-blue-300 bg-opacity-50">
-                <img className='w-[385px] rounded-t-xl' src="/assits/images/class1.webp" alt="class" />
+                <img className='w-[385px] rounded-t-xl' src="/assits/images/class.webp" alt="class" />
                 <div className=" p-8 mb-4">
-                    <Heading level={3}>Our Learning Environment</Heading>
-                    <Paragraph variant='paraimg'>Edify College of IT features a cutting-edge learning environment, blending advanced technology and hands-on educational methods to prepare students for the IT industry.</Paragraph>
+                    <Heading level={3}>Empowering Digital Skills</Heading>
+                    <Paragraph variant='paraimg'>Edify College of IT combines advanced tech and hands-on learning in modern settings, preparing students for the evolving IT world.</Paragraph>
                 </div>
             </div>
             <div className=" w-[40%] mx-[7.5%] ">
                 <div className=" p-3 text-center inline-flex items-center justify-center w-16 h-16 mb-6 shadow-lg rounded-full bg-slate-300">
-                    <img src="/assits/images/gif1.gif" alt="gif" />
+                    <img src="/assits/images/gif2.gif" alt="gif" />
                 </div>
                 <div className="">
-                    <Heading level={1}>Working with us is a pleasure</Heading>
+                    <Heading level={1}>Education Focus</Heading>
                 </div>
                 <div className="">
-                    <Paragraph variant='paraAbout'>Our vision is cultivating a dynamic learning environment where innovation, adaptability, and excellence converge. We aspire to be the catalyst for transformative education, equipping students with the technical skills, resilience, and creativity required to navigate and excel in the diverse challenges of the modern world.</Paragraph>
-                    <Paragraph variant='paraAbout'>In our vision, a vibrant community of learners is inspired to push boundaries, committed to continuous improvement, and driven by a passion for leveraging technology for positive change.</Paragraph>
+                    <Paragraph variant='paraAbout'>Our journey began with the goal of creating a dynamic learning environment that crosses conventional boundaries, motivated by a passion for affecting the direction of technology. As you explore our official website, you'll discover a vibrant community of learners, passionate educators, and a wealth of resources designed to fuel your curiosity and ambition</Paragraph>
+                    <Paragraph variant='paraAbout'>Where learning knows no bounds, with the story of innovation and education unfolding. Join us on this exciting journey of knowledge and growth.</Paragraph>
                 </div>
             </div>
 
